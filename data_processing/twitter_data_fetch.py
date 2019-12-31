bb5415081f39964564e442f60aceb8802b0f7296
Ghoul <rodrigueznatalandy@gmail.com>
2019-12-31 01:40:18 -0400

    added  necessary data to connect stream. Miss lim

--- data_processing/twitter_data_fetch.py
+++ data_processing/twitter_data_fetch.py
@@ -1,4 +1,5 @@
-import tweepy
+import tweepy, sys
+from tweepy_au import *
 
 # consumer_key, consumer_secret = "KEY HERE", "SECRET HERE"
 # access_token, access_token_secret = "token here" ,"secret token here"
@@ -16,22 +17,44 @@ import tweepy
 
 class DorianStreamListener(tweepy.StreamListener):
 
+    def __init__(self, output=sys.stdout):
+        super(DorianStreamListener, self).__init__()
+        self.output = output
+
     def on_status(self, status):
-        print(status.text)
+        print(status.text, file=self.output)
 
     def on_error(self, status_code):
+        print(f'Error encountered:{status_code}')
         if status_code == 420:
             return False
 
         else:
             return True
 
-def establish_stream(consumer_key, consumer_secret, tracking="Dorian"):
+def establish_stream(consumer_key=consumer_key, consumer_secret=consumer_key, tracking="Dorian"):
     
     auth = tweepy.AppAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     stream_listener = DorianStreamListener()
     stream_listener = tweepy.Stream(auth=api.auth, listener=stream_listener)
     stream_listener.filter(track=tracking, is_async=True)#will run on its own independent thread
 
-    return stream_listener
+    return  auth, stream_listener
+
+def init_api():
+
+    auth ,stream = establish_stream()
+
+    try:
+        print("Stream Started")
+        stream.sample(languages=['en'])
+    except KeyboardInterrupt:
+        print("Stopped")
+    
+    finally:
+        print("Stream Ended")
+        stream.disconnect()
+        
